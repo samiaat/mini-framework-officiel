551fa3415fa11d0b63b605e3c86f5de32d31b31f
samiaat <jaddidi026@gmail.com>
2025-08-08 02:20:52 +0100

    abstraction DOM done

---
+++ framework/dom.js
@@ -0,0 +1,72 @@
+import { applyEventHandlers } from './event.js';
+
+export const createElement = (tag, props, ...children) => {
+  return {
+    tag,
+    props: props || {},
+    children: children.flat(),
+  };
+};
+
+
+export const render = (vnode) => {
+  if (vnode === null || vnode === undefined || typeof vnode === 'boolean') {
+    return document.createTextNode('');
+  }
+
+  if (typeof vnode === 'string' || typeof vnode === 'number') {
+    return document.createTextNode(vnode.toString());
+  }
+
+  const { tag, props, children } = vnode;
+
+  const element = document.createElement(tag);
+
+  applyEventHandlers(element, props);
+
+  for (const [key, value] of Object.entries(props)) {
+    if (key.startsWith('on')) {
+      continue; 
+    }
+
+    if (key === 'ref') {
+      // Handle ref callbacks to get a direct reference to the DOM element.
+      if (typeof value === 'function') {
+        value(element);
+      }
+    } else if (key === 'checked' || key === 'value' || key === 'disabled' || key === 'autofocus') {
+      element[key] = value;
+    } else {
+      element.setAttribute(key, value);
+    }
+  }
+
+  // Recursively 
+  for (const child of children) {
+    element.appendChild(render(child));
+  }
+
+  return element;
+};
+
+export const mount = (node, target) => {
+  target.innerHTML = '';
+  target.appendChild(node);
+  return node;
+};
+
+
+export const createApp = (component, target) => {
+    let currentVNode = component();
+    let rootNode = render(currentVNode);
+    mount(rootNode, target);
+
+    return () => {
+        const newVNode = component();
+        const newRootNode = render(newVNode);
+        rootNode.replaceWith(newRootNode);
+
+        currentVNode = newVNode;
+        rootNode = newRootNode;
+    };
+};
---
+++ framework/event.js
@@ -0,0 +1,7 @@
+export function applyEventHandlers(element, props) {
+  for (const [key, value] of Object.entries(props)) {
+    if (key.startsWith('on') && typeof value === 'function') {
+      element[key.toLowerCase()] = value;// bach manstaamloch addeventlisner
+    }
+  }
+}
