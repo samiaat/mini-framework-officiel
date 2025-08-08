8dba41b01ad3c4505c8a9c3ec351e539b8613d68
samiaat <jaddidi026@gmail.com>
2025-08-08 03:10:16 +0100

    router done read good redux

---
+++ framework/router.js
@@ -0,0 +1,24 @@
+export const createRouter = (store) => {
+  const getRoute = () => {
+    const hash = window.location.hash;
+    switch (hash) {
+      case '#/active':
+        return 'active';
+      case '#/completed':
+        return 'completed';
+      default:
+        return 'all';
+    }
+  };
+
+  const handleHashChange = () => {
+    const route = getRoute();
+    store.dispatch({
+      type: 'SET_FILTER',
+      payload: route,
+    });
+  };
+  window.onhashchange = handleHashChange;
+  
+  handleHashChange();
+};
